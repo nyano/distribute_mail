0872877157c0ca1a820bceac0d664a7013d511b2
nyano <569942089@qq.com>
2023-09-06 15:27:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import argparse
 import configparser
 import os
@@ -36,9 +37,9 @@ def send_email(server, sender, receiver, subject, content, files):
 
     for file in files:
         file_name = os.path.basename(file)  # 获取文件名
-        with open(file， 'rb') as attachment:
+        with open(file, 'rb') as attachment:
             part = MIMEApplication(attachment.read())
-        part.add_header('Content-Disposition'， 'attachment', filename=file_name)
+        part.add_header('Content-Disposition', 'attachment', filename=file_name)
         msg.attach(part)
 
     if server['smtp_ssl'].lower() == 'true':
@@ -53,12 +54,49 @@ def send_email(server, sender, receiver, subject, content, files):
 # 解析命令行参数
 def parse_arguments():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-help', action='store_true', help='帮助')
     parser.add_argument('-sender', action='store_true', help='发送有config.ini的子文件邮件')
     return parser.parse_args()
 
 # 主函数
 def main():
     args = parse_arguments()
+    
+    if args.help:
+        print("""
+自动发送邮件装置  
+1.首先请确认当前文件夹有config.ini  
+[server]  
+    smtp_server =       # SMTP服务器  
+    smtp_port =         # SMTP端口  
+    smtp_ssl =          # True False 使用SSL加密  
+    smtp_user =         # 发件人账号  
+    smtp_password =     # 发件人密码  
+[sender]  
+    sender_name =        # 发件人名称  
+    sender_address =     # 发件人地址  
+[body]  
+    subject =           # 邮件主题  
+    content =           # 邮件内容  
+
+2.然后创建子文件夹，子文件夹中包含config.ini文件，才会识别该文件夹，内容如下。to_1、to_2为第一个收件人、第二个收件人，以此类推。 一个子目录为多个收件人但发送相同的附件，如果要不同的附件则创建不同的子目录。
+把附件直接放在该文件夹内即可。  
+[to_1]  
+    receiver_name =     # 收件人名称  
+    receiver_address =  # 收件人地址  
+[to_2]  
+    receiver_name =     # 收件人名称  
+    receiver_address =  # 收件人地址  
+3.如果某个目录不想发了，则创建一个not子目录，直接把子文件夹拖进去即可，不会识别not子目录。  
+4.直接运行程序会显示当前有效的设置，以及有效的子文件夹内容。确认下发送的信息是否正确，不要发错！！！  
+5.如果要发送邮件，则后面加上-sender参数即可。帮助信息为-help参数。  
+6.发送后，保存发送结果到log.log文件中。如有错误，请阅读log.log。  
+7.注意！！！config.ini中的发件人邮箱密码没有做加密，我想想是自用的，所以没做加密，请不要使用后传播config.ini！！！ 
+8.所有邮件的邮件主题和内容相同，仅收件人和附件不一样。
+
+PS.我本来想写邮件内容中添加变量的，想想算了，直接把想区分的直接写个文件放附件里不就行了？
+""")
+        exit(0)
 
     if not args.sender:
         print('先来确认一下信息\n')
@@ -77,7 +115,7 @@ def main():
 
     # 遍历所有子文件夹
     for folder in os.listdir('.'):
-        if os.path.isdir(folder):
+        if folder != 'not' and os.path.isdir(folder):
             folder_path = os.path.abspath(folder)
             # folder_path = os.path.join(folder_path, '')
             config, files = get_folder_info(folder)
